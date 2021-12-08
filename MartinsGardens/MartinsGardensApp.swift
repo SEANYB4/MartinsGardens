18a69e0cdc2060459af81aee5cc4480c1c460d46
Sean Bain <75031999+SEANYB4@users.noreply.github.com>
2021-12-08 08:05:53 +0000

    Initial Commit

---
+++ MartinsGardens/MartinsGardensApp.swift
@@ -0,0 +1,17 @@
+//
+//  MartinsGardensApp.swift
+//  MartinsGardens
+//
+//  Created by Sean Bain on 08/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct MartinsGardensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
